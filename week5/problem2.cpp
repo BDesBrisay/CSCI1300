1e7c6b1846cfc663320a39dc94f65f0fe5a4a6d0
BDesBrisay <bryce@desbrisay.com>
2017-09-27 18:55:45 -0600

    removed commented errors

--- week5/problem2.cpp
+++ week5/problem2.cpp
@@ -41,35 +41,3 @@ int main() {
   cout<<"The wind chill is "<<windChillCalculator(T, V)<<" degrees F."<<endl;
   printWindChill(30.0, 5.0, 8.0, 1.0);
 }
-
-/*
-[-10.0]
-  Incorrect Answer. First difference near character 60.
-  Expected Output: The wind chill is -37.94 degrees F for an airtemperature of -8.63 degrees F and a wind speed of 31.00 mph. ,
-  Your Output: The wind chill is -37.94 degrees F for an airtemperature of -8.63 degreed F and a wind speed of 31.0 mph.
-  The wind chill is -38.29 degrees F for an airtemperature of -8.63 degreed F and a wind speed of 32.0 mph.
-[-10.0]
-  Incorrect Answer. First difference near character 61.
-  Expected Output: The wind chill is -36.39 degrees F for an airtemperature of -11.81 degrees F and a wind speed of 18.00 mph.
-    The wind chill is -36.67 degrees F for an airtemperature of -11.81 degrees F and a wind speed of 18.50 mph.
-    The wind chill is -36.95 degrees F for an airtemperature of -11.81 degrees F and a wind speed of 19.00 mph.
-    The wind chill is -37.22 degrees F for an airtemperature of -11.81 degrees F and a wind speed of 19.50 mph.
-    The wind chill is -37.49 degrees F for an airtemperature of -11.81 degrees F and a wind speed of 20.00 mph. ,
-  Your Output: The wind chill is -36.39 degrees F for an airtemperature of -11.81 degreed F and a wind speed of 18.0 mph.
-    The wind chill is -36.95 degrees F for an airtemperature of -11.81 degreed F and a wind speed of 19.0 mph.
-    The wind chill is -37.49 degrees F for an airtemperature of -11.81 degreed F and a wind speed of 20.0 mph.
-[-10.0]
-  Incorrect Answer. First difference near character 18.
-  Expected Output: The wind chill is -45.92 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 17.92 mph.
-    The wind chill is -46.05 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 18.12 mph.
-    The wind chill is -46.17 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 18.32 mph.
-    The wind chill is -46.29 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 18.52 mph.
-    The wind chill is -46.41 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 18.72 mph.
-    The wind chill is -46.53 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 18.92 mph.
-    The wind chill is -46.65 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 19.12 mph.
-    The wind chill is -46.77 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 19.32 mph.
-    The wind chill is -46.88 degrees F for an airtemperature of -19.18 degrees F and a wind speed of 19.52 mph. ,
-  Your Output: The wind chill is -45.34 degrees F for an airtemperature of -19.18 degreed F and a wind speed of 17.0 mph.
-    The wind chill is -45.97 degrees F for an airtemperature of -19.18 degreed F and a wind speed of 18.0 mph.
-    The wind chill is -46.58 degrees F for an airtemperature of -19.18 degreed F and a wind speed of 19.0 mph.
-*/
